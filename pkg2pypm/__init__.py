084bb01fd6c176104d77224956ef10c12b5fda70
Grigory Petrov <grigory.v.p@gmail.com>
2012-10-16 19:23:10 +0400

    Added commandline handling.

--- pkg2pypm/__init__.py
+++ pkg2pypm/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # coding:utf-8 vi:et:ts=2
 
+import argparse
+
 def main() :
-  print( "foo" )
+  oParser = argparse.ArgumentParser( description = "PYPI to PYPM converter" )
+  oParser.add_argument( 'source', help = "PYPI .tar.gz file or directory" )
+  oParser.add_argument( 'target', help = "PYPM .pypm file to create" )
+  oArgs = oParser.parse_args()
+  if oArgs.source.isfile() and oArgs.source.endswith( 'tar.gz' ) :
+    pass
 
