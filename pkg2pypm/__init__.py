7f7ffc8aea17d4b601c2d1aab7f81fe997b30545
Grigory Petrov <grigory.v.p@gmail.com>
2012-10-17 00:38:31 +0400

    Added second temp dir to create .pypm structure.

--- pkg2pypm/__init__.py
+++ pkg2pypm/__init__.py
@@ -44,30 +44,33 @@ def main() :
   try :
     ##  Used to extract ".tar.gz" into or copy package directory to run
     ##  it's 'setup.py' and create metadata.
-    sDirTmp = tempfile.mkdtemp()
+    sDirTmpPkg = tempfile.mkdtemp()
+    ##  Used to create PYPM package directory structure before packing
+    ##  it into .pypm file that is actually a .zip archive.
+    sDirTmpPypm = tempfile.mkdtemp()
     oParser = argparse.ArgumentParser( description = ABOUT_APP )
     oParser.add_argument( 'source', help = "PYPI .tar.gz file or directory" )
     oParser.add_argument( 'target', help = "PYPM .pypm file to create" )
     oArgs = oParser.parse_args()
     if os.path.isfile( oArgs.source ) :
       assert oArgs.source.endswith( 'tar.gz' ), "Source file must be .tar.gz"
-      sFileTmp = '{0}/pkg.tar'.format( sDirTmp )
+      sFileTmp = '{0}/pkg.tar'.format( sDirTmpPkg )
       ##  Extract '.tar' from 'tar.gz'
       with open( sFileTmp, 'wb' ) as oDst :
         with gzip.open( oArgs.source ) as oSrc :
           oDst.write( oSrc.read() )
       ##  Extract content of '.tar'
       with tarfile.open( sFileTmp ) as oSrc :
-        oSrc.extractall( sDirTmp )
+        oSrc.extractall( sDirTmpPkg )
       os.remove( sFileTmp )
       ##  Single folder inside temp dir is the package from '.tar.gz'
-      lContent = os.listdir( sDirTmp )
+      lContent = os.listdir( sDirTmpPkg )
       assert len( lContent ) == 1, ".tar must contain one root item"
-      sDirPkg = os.path.join( sDirTmp, lContent[ 0 ] )
+      sDirPkg = os.path.join( sDirTmpPkg, lContent[ 0 ] )
       assert os.path.isdir( sDirPkg ), ".tar must contain directory"
     else :
-      distutils.dir_util.copy_tree( oArgs.source, sDirTmp )
-      sDirPkg = sDirTmp
+      distutils.dir_util.copy_tree( oArgs.source, sDirTmpPkg )
+      sDirPkg = sDirTmpPkg
     ##  If package don't have '.egg-info' subdir with metadata than create it.
     if not getDirMeta( sDirPkg ) :
       subprocess.check_return( [
@@ -81,10 +84,11 @@ def main() :
     ##  Read package metadata.
     with open( os.path.join( sDirMeta, 'PKG-INFO' ), 'rb' ) as oFile :
       ##! Names will be lowercase.
-      mMetaPypi = dict( rfc822.Message( oFile ).items() )
+      mMetaPkg = dict( rfc822.Message( oFile ).items() )
     ##  Create metadata for PYPM.
-    mMetaPypm = convertMetadata( mMetaPypi )
+    mMetaPypm = convertMetadata( mMetaPkg )
     print( mMetaPypm )
   finally :
-    shutil.rmtree( sDirTmp, ignore_errors = True )
+    shutil.rmtree( sDirTmpPkg, ignore_errors = True )
+    shutil.rmtree( sDirTmpPypm, ignore_errors = True )
 
