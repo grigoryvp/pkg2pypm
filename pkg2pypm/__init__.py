03c76c4b1e034873d97de488bd740f2265538ad3
Grigory Petrov <grigory.v.p@gmail.com>
2013-06-14 11:44:10 +0400

    Refactored to match updated coding standard.

--- pkg2pypm/__init__.py
+++ pkg2pypm/__init__.py
@@ -23,31 +23,31 @@ import re
 import info
 
 
-def tarWriteStr( o_archive, s_name, s_data ) :
+def tarWriteStr( o_archive, s_name, s_data ):
   oInfo = tarfile.TarInfo( name = s_name )
   oInfo.size = len( s_data )
   o_archive.addfile( oInfo, StringIO.StringIO( s_data ) )
 
 
-def getDirMeta( s_dir ) :
-  for sDir in os.listdir( s_dir ) :
+def getDirMeta( s_dir ):
+  for sDir in os.listdir( s_dir ):
     sDir = os.path.join( s_dir, sDir )
-    if os.path.isdir( sDir ) and sDir.endswith( '.egg-info' ) :
+    if os.path.isdir( sDir ) and sDir.endswith( '.egg-info' ):
       return sDir
 
 
-def getOneFile( * l_args ) :
+def getOneFile( * l_args ):
   sPath = os.path.join( * l_args )
   lContent = os.listdir( sPath )
   assert len( lContent ) == 1, "\"{0}\" must have one item".format( sPath )
   return os.path.join( sPath, lContent[ 0 ] )
 
 
-def convertMetadata( m_metadata ) :
+def convertMetadata( m_metadata ):
   ##? Architecure need to be passed to script.
   ##? Python version need to be passed to script.
-  mDst = { 'pkg_version' : 1, 'osarch' : 'win32-x86', 'pyver' : '2.7' }
-  def convertRecord( s_name, s_sourcename = None ) :
+  mDst = { 'pkg_version': 1, 'osarch': 'win32-x86', 'pyver': '2.7' }
+  def convertRecord( s_name, s_sourcename = None ):
     mDst[ s_name ] = m_metadata.get( s_sourcename or s_name )
   convertRecord( 'maintainer' )
   convertRecord( 'description' )
@@ -64,8 +64,8 @@ def convertMetadata( m_metadata ) :
   return mDst
 
 
-def main() :
-  try :
+def main():
+  try:
     ##  Used to extract ".tar.gz" into or copy package directory to run
     ##  it's 'setup.py' and create metadata.
     sDirTmpPkg = tempfile.mkdtemp()
@@ -77,16 +77,16 @@ def main() :
     oArgs.source = os.path.abspath( oArgs.source )
     oArgs.target = os.path.abspath( oArgs.target )
     assert not os.path.isdir( oArgs.target ), "Target must be file, not dir"
-    if os.path.isfile( oArgs.source ) :
+    if os.path.isfile( oArgs.source ):
       assert oArgs.source.endswith( 'tar.gz' ), "Source file must be .tar.gz"
-      with tarfile.TarFile.gzopen( oArgs.source, 'rb' ) as oArchive :
+      with tarfile.TarFile.gzopen( oArgs.source, 'rb' ) as oArchive:
         oArchive.extractall( sDirTmpPkg )
       ##  Single folder inside temp dir is the package from '.tar.gz'
       lContent = os.listdir( sDirTmpPkg )
       assert len( lContent ) == 1, ".tar must contain one root item"
       sDirPkg = os.path.join( sDirTmpPkg, lContent[ 0 ] )
       assert os.path.isdir( sDirPkg ), ".tar must contain directory"
-    else :
+    else:
       distutils.dir_util.copy_tree( oArgs.source, sDirTmpPkg )
       sDirPkg = sDirTmpPkg
     ##  Create binary distribution in temp folder package was copied into.
@@ -100,17 +100,17 @@ def main() :
       ##! For 'python' to resolve.
       shell = True, stderr = subprocess.STDOUT )
     ##  Create PYPM package, it's a .tar.gz archive:
-    with tarfile.TarFile.gzopen( oArgs.target, 'w' ) as oTarget :
+    with tarfile.TarFile.gzopen( oArgs.target, 'w' ) as oTarget:
       ##  Read source package data
-      with zipfile.ZipFile( getOneFile( sDirPkg, 'dist' ) ) as oSource :
+      with zipfile.ZipFile( getOneFile( sDirPkg, 'dist' ) ) as oSource:
         ##  'bdist' command created 'bdist' dir with '.zip' archive that
         ##  contains dir like 'Python27' that contains 'Lib' and 'Scripts'
         ##  subdirs. PYPM pckage must contain 'data.tar.gz' archive that
         ##  contains 'Lib' and 'Scripts' as top level dirs. So, repack.
         oDataBits = StringIO.StringIO()
-        mArgs = { 'fileobj' : oDataBits, 'mode' : 'w' }
-        with tarfile.TarFile.gzopen( name = None, ** mArgs ) as oData :
-          for oFile in oSource.filelist :
+        mArgs = { 'fileobj': oDataBits, 'mode': 'w' }
+        with tarfile.TarFile.gzopen( name = None, ** mArgs ) as oData:
+          for oFile in oSource.filelist:
             ##! Skip first dir in source archive, it's like 'Python27'.
             sName = re.sub( r'^[^/\\]+(/|\\)', '', oFile.filename )
             tarWriteStr( oData, sName, oSource.read( oFile.filename ) )
@@ -119,13 +119,13 @@ def main() :
       ##  Read source package metadata.
       sDirMeta = getDirMeta( sDirPkg )
       assert sDirMeta, "No .egg-info metadir found"
-      with open( os.path.join( sDirMeta, 'PKG-INFO' ), 'rb' ) as oFile :
+      with open( os.path.join( sDirMeta, 'PKG-INFO' ), 'rb' ) as oFile:
         ##  And write it into target package.
         ##! Names will be lowercase.
         sMetadata = convertMetadata( dict( rfc822.Message( oFile ).items() ) )
         tarWriteStr( oTarget, 'info.json', json.dumps( sMetadata ) )
-  except Exception as oEx :
+  except Exception as oEx:
     print( "error: \"{0}\"".format( oEx.message ) )
-  finally :
+  finally:
     shutil.rmtree( sDirTmpPkg, ignore_errors = True )
 
